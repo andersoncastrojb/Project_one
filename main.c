45374b83152e9fe2c2d27ff9548d63c5bd73456b
andersoncastrojb <andersoncastb@gmail.com>
2021-03-15 00:38:44 -0500

    Porject_01

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("This program is for numerical  calculation\r\n");
+    printf("Sum, subtraction, multiplication, division\r\n");
+
+
+
+
+    return(0);
+}
+
+
