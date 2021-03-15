c0690d3057df64743870d1d6ab2d9a71c2761cb9
andersoncastrojb <andersoncastb@gmail.com>
2021-03-15 03:37:00 -0500

    Division agregada

--- main.c
+++ main.c
@@ -13,6 +13,10 @@ float multiplication(int a, int b){
     return(a*b);
 }
 
+float division(int a, int b){
+    return( (float) a/b );
+}
+
 int main(void){
 
     int a = 0, b = 0, sal = 0;
@@ -45,6 +49,12 @@ int main(void){
                sal = 1;
                printf("Result of the multiplication is: %f\r\n",resultado); 
           }
+          else if (operacion == 4){
+               resultado = division(a,b);
+               sal = 1;
+               printf("Result of the division is: %f\r\n",resultado); 
+          }
+
           else{
                 printf("Choose: Sum[1], subtraction[2], multiplication[3], division[4]\r\n--> ");
                 sal = 0;
